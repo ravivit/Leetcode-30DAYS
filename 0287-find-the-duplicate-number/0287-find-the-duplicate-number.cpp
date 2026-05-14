ecdfff494d3c0bc9a394949a1af8e50a9df1bf31
RAVI KASAUDHAN <91520095+ravivit@users.noreply.github.com>
2026-05-14 12:40:58 +0530

    Time: 0 ms (100%), Space: 65.1 MB (68.66%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -3,24 +3,26 @@ public:
     int findDuplicate(vector<int>& nums) {
     
       int slow  = nums[0];
-      int fast  = nums[0];
+      int fast  = nums[0];   // variuble declare 
 
       do {
-        slow = nums[slow];
-        fast = nums[nums[fast]];
+        slow = nums[slow];      // ye  1 point chelega
+        fast = nums[nums[fast]]; // ye 2 point chaalega 
        }
 
-       while (slow!= fast);
-       slow = nums[0];
+       while (slow!= fast);     // jb tk slow and start same nahi ho jate 
+       slow = nums[0];  // ek pointer dobara start pr jayega 
 
      while (slow!= fast ) {
 
-        slow = nums[slow];
-        fast = nums[fast];
+        slow = nums[slow]; // now dono pointer ek ek point chalege jha
+        fast = nums[fast]; // jaha milege  vhi duplicate numnber start hoga
+
      }
 
      return slow;
 }
 
-    
 };
+
+    
