0a0a5954ef99fd85c6f791e5a4b41c9b0be1d762
RAVI KASAUDHAN <91520095+ravivit@users.noreply.github.com>
2026-08-03 14:26:40 +0530

    Time: 0 ms (100%), Space: 7.7 MB (97.51%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -1,43 +1,27 @@
 class Solution {
 public:
+    int squareSum(int n) {
+        int sum = 0;
 
-int squareSum(int n) {
+        while (n > 0) {
+            int digit = n % 10;
+            sum += digit * digit;
+            n /= 10;
+        }
 
-    int sum = 0;
-    while (n>0) {
-
-        int digit = n%10;
-        sum += digit * digit;
-
-        n = n/10;
+        return sum;
     }
-
-    return sum;
-}
-
-
-
-bool isHappy(int n) {
-
-    int slow = n;
-    int fast = n;
-
-
-    do {
-        slow =  squareSum(slow);
-        fast =  squareSum(squareSum(fast));
-}
- while(slow != fast);
- return slow == 1;
-
-        
-     
-
     
 
-    }
+    bool isHappy(int n) {
+        int slow = n;
+        int fast = n;
 
+        do {
+            slow = squareSum(slow);
+            fast = squareSum(squareSum(fast));
+        } while (slow != fast);
+
+        return slow == 1;
+    }
 };
-     
-
-    
